a7ec3b100f15d1598317b34c558423386a13cf92
Richard Clamp <richardc@unixbeard.net>
2024-03-04 16:06:54 +0000

    Day 2 Part 1: Parse games and filter to matching
    As before, quite the bloiderplate in parsing the data into structures,
    then a simple scan (for part 1 at least)
    
    Adds anyhow as a way of  making the fromstr parsing less painful

---
+++ src/bin/02.rs
@@ -0,0 +1,106 @@
+use std::str::FromStr;
+
+use anyhow::bail;
+
+advent_of_code::solution!(2);
+
+struct Round {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl FromStr for Round {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut r = Round {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+
+        for die in s.trim().split(',') {
+            if let Some((count, color)) = die.trim().split_once(' ') {
+                match color {
+                    "red" => r.red = count.parse().unwrap(),
+                    "green" => r.green = count.parse().unwrap(),
+                    "blue" => r.blue = count.parse().unwrap(),
+                    _ => {}
+                }
+            } else {
+                bail!("parsing die {}", die)
+            }
+        }
+        Ok(r)
+    }
+}
+
+struct Game {
+    id: u32,
+    rounds: Vec<Round>,
+}
+
+impl FromStr for Game {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some((game, parts)) = s.split_once(':') {
+            if let Some((_, id)) = game.split_once(' ') {
+                Ok(Game {
+                    id: id.parse().unwrap(),
+                    rounds: parts.split(';').map(|r| r.parse().unwrap()).collect(),
+                })
+            } else {
+                bail!("Bad format")
+            }
+        } else {
+            bail!("Games")
+        }
+    }
+}
+
+impl Game {
+    fn is_legal(self: &Self, red: u32, green: u32, blue: u32) -> bool {
+        self.rounds
+            .iter()
+            .all(|r| r.red <= red && r.green <= green && r.blue <= blue)
+    }
+}
+
+fn parse(input: &str) -> Vec<Game> {
+    input.lines().map(|l| Game::from_str(l).unwrap()).collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let games = parse(input);
+    let legal = games.iter().filter_map(|x| {
+        if x.is_legal(12, 13, 14) {
+            Some(x.id)
+        } else {
+            None
+        }
+    });
+    Some(legal.sum())
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(8));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
