b89b142931d6bdbe4637a99181f20dd9cd628fd4
Richard Clamp <richardc@unixbeard.net>
2024-03-04 13:15:27 +0000

    Day1 Part2  Scan for sequences too
    Here we scan for "one", "two", etc and if found add that to our found
    digits and skip the correct amount of spaces.
    
    Currntly broken, new output value is 54627 which aoc says is too low for
    the data.  Works for the given example though (see test case)
    
    Hmm.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -27,8 +27,67 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn extract_word_digits(line: &str) -> (u8, u8) {
+    let mut digits: Vec<u8> = vec![];
+    let mut b = line.as_bytes();
+    while b.len() > 0 {
+        let mut skip = 1;
+        match b {
+            b if b.starts_with(b"one") => {
+                skip = 3;
+                digits.push(1);
+            }
+            b if b.starts_with(b"two") => {
+                skip = 3;
+                digits.push(2);
+            }
+            b if b.starts_with(b"three") => {
+                skip = 5;
+                digits.push(3);
+            }
+            b if b.starts_with(b"four") => {
+                skip = 4;
+                digits.push(4);
+            }
+            b if b.starts_with(b"five") => {
+                skip = 4;
+                digits.push(5);
+            }
+            b if b.starts_with(b"six") => {
+                skip = 3;
+                digits.push(6);
+            }
+            b if b.starts_with(b"seven") => {
+                skip = 5;
+                digits.push(7);
+            }
+            b if b.starts_with(b"eight") => {
+                skip = 5;
+                digits.push(8);
+            }
+            b if b.starts_with(b"nine") => {
+                skip = 4;
+                digits.push(9);
+            }
+            b if b[0].is_ascii_digit() => digits.push(b[0] - b'0'),
+            _ => {}
+        }
+        b = &b[skip..];
+    }
+
+    (*digits.first().unwrap(), *digits.last().unwrap())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let (first, last) = extract_word_digits(line);
+                u32::from(first * 10 + last)
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -43,7 +102,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(281));
     }
 }
