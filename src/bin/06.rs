f2d3be05add96086073b0cd539816eb65de71457
Richard Clamp <richardc@unixbeard.net>
2024-03-05 16:02:57 +0000

    Day 6 Part 2: it's one Race
    A search for the extents here would be far quicker, but we bruteforce
    our way to the right answer in under a second on a debug build.
    
    Part 1: 3317888 (31.1µs)
    Part 2: 24655068 (703.1ms)

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,14 +2,14 @@ advent_of_code::solution!(6);
 
 #[derive(Debug)]
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Race {
-    fn winning_starts(&self) -> Vec<u32> {
+    fn winning_starts(&self) -> Vec<u64> {
         let mut starts = Vec::new();
-        for start in 0..self.time {
+        for start in 1..self.time {
             let travelled = (self.time - start) * start;
             if travelled > self.distance {
                 starts.push(start);
@@ -40,18 +40,44 @@ fn parse(s: &str) -> Vec<Race> {
         .collect()
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let races = parse(input);
     Some(
         races
             .iter()
-            .map(|r| r.winning_starts().len() as u32)
+            .map(|r| r.winning_starts().len() as u64)
             .product(),
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn parse2(s: &str) -> Race {
+    let Some((times, distances)) = s.split_once('\n') else {
+        panic!("malformed data")
+    };
+    let Some((_, time)) = times.split_once(':') else {
+        panic!(":");
+    };
+    let Some((_, dist)) = distances.split_once(':') else {
+        panic!(":");
+    };
+
+    Race {
+        time: time
+            .as_bytes()
+            .iter()
+            .filter(|b| b.is_ascii_digit())
+            .fold(0, |acc, d| acc * 10 + (*d - b'0') as u64),
+        distance: dist
+            .as_bytes()
+            .iter()
+            .filter(|b| b.is_ascii_digit())
+            .fold(0, |acc, d| acc * 10 + (*d - b'0') as u64),
+    }
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let race = parse2(input);
+    Some(race.winning_starts().len())
 }
 
 #[cfg(test)]
@@ -76,6 +102,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71_503));
     }
 }
