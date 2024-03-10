2afa20eee23c6e04521e06cfe252930a10c80494
Richard Clamp <richardc@unixbeard.net>
2024-03-10 10:16:21 +0000

    Day 12 Part 2 Copied some Dynamic Programming from reddit
    Did have some scribbles down on how to approach this dynamically,
    building up from the known unknowns, but couldn't quite figure out how
    to translate it into code.
    
    Ended up going to the AOC reddit and copying this:
    
    https://github.com/manhunto/advent-of-code-rs/blob/master/src/solutions/day12.rs
    
    Have added enough tests and comments to convince myself I understand how
    this works, but I'm still dissatisfied that I didn't solve this myself.
    
    One to come back to I guess.

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,67 +1,76 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(12);
 
-fn matches_record(s: &[u8], record: &[usize]) -> bool {
-    let pattern: Vec<usize> = s
-        .iter()
-        .group_by(|&&b| b == b'#')
-        .into_iter()
-        .filter_map(|(s, g)| if s { Some(g.count()) } else { None })
-        .collect();
-    record == pattern
-}
-
-fn generate_records(pattern: &str) -> RecordIter {
-    let pattern = pattern.bytes().collect_vec();
-    let indexes = (0..pattern.len())
-        .filter(|&i| pattern[i] == b'?')
-        .collect_vec();
-    let max = 1 << indexes.len();
-    RecordIter {
-        pattern,
-        indexes,
-        index: 0,
-        max,
+fn possible_completions<'a, 'b>(
+    springs: &'a [u8],
+    summary: &'b [usize],
+    cache: &mut HashMap<(&'a [u8], &'b [usize]), usize>,
+) -> usize {
+    let cache_key = (springs, summary);
+    if let Some(&result) = cache.get(&cache_key) {
+        return result;
     }
-}
 
-struct RecordIter {
-    pattern: Vec<u8>,
-    indexes: Vec<usize>,
-    index: usize,
-    max: usize,
-}
+    if summary.is_empty() {
+        // We're out of groups to match, so this will be one match if
+        // none of the remaining springs are clearly failed.
+        let any_failed = springs.iter().any(|&b| b == b'#');
+        let result = if any_failed { 0 } else { 1 };
 
-impl Iterator for RecordIter {
-    type Item = Vec<u8>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.max {
-            let mut pattern = self.pattern.clone();
-            for i in 0..self.indexes.len() {
-                pattern[self.indexes[i]] = if self.index & (1 << i) != 0 {
-                    b'#'
-                } else {
-                    b'.'
-                };
-            }
-            self.index += 1;
-            return Some(pattern);
-        }
-        None
+        cache.insert(cache_key, result);
+        return result;
     }
+
+    let needed_space = summary.iter().sum::<usize>() + summary.len() - 1;
+    if springs.len() < needed_space {
+        // The summary specifies more springs than we have, so no match.
+        cache.insert(cache_key, 0);
+        return 0;
+    }
+
+    if springs[0] == b'.' {
+        // Starting with success is about the same as starting with failure, but
+        // less instructive, we can just skip
+        return possible_completions(&springs[1..], summary, cache);
+    }
+
+    // Check the current group
+    let group = summary[0];
+    // All the springs must be failed or unknown
+    let all_non_operational = springs[..group].iter().all(|&b| b != b'.');
+    let end = (group + 1).min(springs.len());
+
+    let mut solutions = 0;
+
+    // If all the current group are failed or unknown -
+    // and: There is a next spring and it's not failed
+    // or: We're at the end of the spring list
+    if all_non_operational
+        && ((springs.len() > group && springs[group] != b'#') || (springs.len() <= group))
+    {
+        // We can recurse and testing the next group of springs
+        solutions += possible_completions(&springs[end..], &summary[1..], cache);
+    }
+
+    // Starting with unknown, we can recurse and scan again just after
+    if springs[0] == b'?' {
+        solutions += possible_completions(&springs[1..], summary, cache)
+    }
+
+    cache.insert(cache_key, solutions);
+    solutions
 }
 
 fn num_completions(s: &str) -> usize {
     let Some((record, summary)) = s.split_once(' ') else {
         unreachable!("no space in line");
     };
-    let summary: Vec<usize> = summary.split(',').map(|v| v.parse().unwrap()).collect();
+    let summary: Vec<usize> = summary.split(',').filter_map(|v| v.parse().ok()).collect();
 
-    generate_records(record)
-        .filter(|r| matches_record(r, &summary))
-        .count()
+    possible_completions(record.as_bytes(), &summary, &mut HashMap::new())
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -85,40 +94,27 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case(b"#.#", &[1,1])]
-    fn test_matches(s: &[u8], r: &[usize]) {
-        assert!(matches_record(s, r))
-    }
-
-    #[test]
-    fn test_generates_single() {
-        assert_eq!(
-            generate_records("?")
-                .map(|v| String::from_utf8(v.to_vec()).unwrap())
-                .collect_vec(),
-            vec![".", "#"]
-        )
-    }
-
-    #[test]
-    fn test_generates_double() {
-        assert_eq!(
-            generate_records("?.?")
-                .map(|v| String::from_utf8(v.to_vec()).unwrap())
-                .collect_vec(),
-            vec!["...", "#..", "..#", "#.#"]
-        )
-    }
-
-    #[test]
-    fn test_unfold() {
-        assert_eq!(unfold(".# 1"), ".#?.#?.#?.#?.# 1,1,1,1,1")
+    #[test_case("? " => 1 ; "Trailing unknown")]
+    #[test_case(". " => 1; "Trailing good")]
+    #[test_case(".?.. " => 1; "Trailing mix of unknown and good")]
+    #[test_case(".# " => 0; "Trailing failed")]
+    #[test_case("??? 10" => 0 ; "Insufficent springs left")]
+    #[test_case(". 1" => 0)]
+    #[test_case("# 1" => 1)]
+    #[test_case(".F 1" => 1)]
+    fn test_num_completions(s: &str) -> usize {
+        num_completions(s)
     }
 
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(21));
+    }
+
+    #[test]
+    fn test_unfold() {
+        assert_eq!(unfold(".# 1"), ".#?.#?.#?.#?.# 1,1,1,1,1")
     }
 
     #[test]
