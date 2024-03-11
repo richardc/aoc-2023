d52400f087c43c7e2a09749a05baea5d91a58646
Richard Clamp <richardc@unixbeard.net>
2024-03-11 13:46:07 +0000

    Day 15 Part 2: Maintain the HASHMAP and report on it
    Tweaked the hoho_hash to take &str and return usize; shouldn't hurt
    too much and is a little more ergonomic to write the rest with as
    there's a lot of using the hash as an index.
    
    For the rest, simply follow the instructions and build up a structure.
    Making an array of Vec was oddly most of the work:
    
        let mut boxes: [Vec<Lens>; 256] = [(); 256].map(|()| Vec::new());
    
    That's one of those dense lines that python folks who think array
    comprehensions are the best thing since sliced bread love.
    
    Would have preferred to have written it more like so:
    
        let mut boxes: [Vec<Lens>; 256] = Default::default();
    
    but that's only supported for N <= 32, as it's actually expanded by hand
    in the library code.

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,25 +1,61 @@
 advent_of_code::solution!(15);
 
-fn hoho_hash(s: &[u8]) -> u32 {
-    let mut acc: u32 = 0;
-    for &b in s {
-        acc += b as u32;
+fn hoho_hash(s: &str) -> usize {
+    let mut acc: usize = 0;
+    for b in s.bytes() {
+        acc += b as usize;
         acc *= 17;
         acc %= 256;
     }
     acc
 }
 
-fn hash_instructions(s: &str) -> u32 {
-    s.trim().split(',').map(|c| hoho_hash(c.as_bytes())).sum()
+fn hash_instructions(s: &str) -> usize {
+    s.trim().split(',').map(hoho_hash).sum()
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     Some(hash_instructions(input))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+#[derive(Debug)]
+struct Lens<'a> {
+    label: &'a str,
+    length: usize,
+}
+
+fn focusing_power(s: &str) -> usize {
+    let mut boxes: [Vec<Lens>; 256] = [(); 256].map(|()| Vec::new());
+
+    for instruction in s.trim().split(',') {
+        if let Some((label, length)) = instruction.split_once('=') {
+            let hash = hoho_hash(&label);
+            let length = length.parse().unwrap();
+            if let Some(index) = boxes[hash].iter().position(|l| l.label == label) {
+                boxes[hash][index].length = length;
+            } else {
+                boxes[hash].push(Lens { label, length });
+            }
+        } else {
+            let label = &instruction[..instruction.len() - 1];
+            let hash = hoho_hash(label);
+            if let Some(index) = boxes[hash].iter().position(|l| l.label == label) {
+                boxes[hash].remove(index);
+            }
+        }
+    }
+
+    let mut power = 0;
+    for (i, b) in boxes.iter().enumerate() {
+        for (j, lens) in b.iter().enumerate() {
+            power += (i + 1) * (j + 1) * lens.length;
+        }
+    }
+    power
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    Some(focusing_power(input))
 }
 
 #[cfg(test)]
@@ -28,7 +64,7 @@ mod tests {
 
     #[test]
     fn test_hoho_hash() {
-        let result = hoho_hash(b"HASH");
+        let result = hoho_hash("HASH");
         assert_eq!(result, 52);
     }
 
@@ -41,6 +77,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(145));
     }
 }
