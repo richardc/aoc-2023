efe2fbc7631a8d6812bb87e03e5ac9a29e4bd79f
Richard Clamp <richardc@unixbeard.net>
2024-03-15 14:24:30 +0000

    Day 21 Part 2: Dumb brute force.
    Convert the coordinates to signed values, so we can consider wandering
    off to the negative extent of our infinite field.
    
    Don't try this at home.

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -2,9 +2,12 @@ use std::collections::HashSet;
 
 advent_of_code::solution!(21);
 
+#[derive(Debug)]
 struct Garden {
-    walls: HashSet<(usize, usize)>,
-    start: (usize, usize),
+    walls: HashSet<(i32, i32)>,
+    width: i32,
+    height: i32,
+    start: (i32, i32),
 }
 
 impl Garden {
@@ -14,26 +17,37 @@ impl Garden {
         for (r, row) in s.lines().enumerate() {
             for (c, b) in row.bytes().enumerate() {
                 if b == b'#' {
-                    walls.insert((r, c));
+                    walls.insert((r as i32, c as i32));
                 }
                 if b == b'S' {
-                    start = (r, c)
+                    start = (r as i32, c as i32)
                 }
             }
         }
-        Self { walls, start }
+        let width = s.lines().next().unwrap().len() as i32;
+        let height = s.lines().count() as i32;
+        Self {
+            walls,
+            start,
+            width,
+            height,
+        }
     }
 
     fn steps(&self, count: usize) -> usize {
-        let mut steps_next: HashSet<(usize, usize)> = HashSet::new();
-        let mut steps: HashSet<(usize, usize)> = HashSet::new();
+        let mut steps_next: HashSet<(i32, i32)> = HashSet::new();
+        let mut steps: HashSet<(i32, i32)> = HashSet::new();
         steps.insert(self.start);
 
         for _ in 0..count {
             steps_next.clear();
             for (r, c) in steps.drain() {
                 for next in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)] {
-                    if self.walls.contains(&next) {
+                    let wrapped = (
+                        next.0.rem_euclid(self.height),
+                        next.1.rem_euclid(self.width),
+                    );
+                    if self.walls.contains(&wrapped) {
                         continue;
                     }
                     steps_next.insert(next);
@@ -51,11 +65,13 @@ pub fn part_one(input: &str) -> Option<usize> {
 
 pub fn part_two(_input: &str) -> Option<usize> {
     None
+    // Some(Garden::new(input).steps(26_501_365))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
     #[test]
     fn test_part_one() {
@@ -63,9 +79,11 @@ mod tests {
         assert_eq!(garden.steps(6), 16);
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    #[test_case(6 => 16)]
+    #[test_case(10 => 50)]
+    #[test_case(50 => 1594)]
+    fn test_part_two(steps: usize) -> usize {
+        let garden = Garden::new(&advent_of_code::template::read_file("examples", DAY));
+        garden.steps(steps)
     }
 }
